9fcd389cca81241c4e4e702e6681c8973366f5e3
Ethan <QustoTemmo@protonmail.com>
2024-04-16 01:33:42 +0100

    Good Progress on Website

---
+++ script.js
@@ -0,0 +1,42 @@
+const questions = document.querySelectorAll(".question");
+const downloadBtn = document.querySelector(".download")
+const upBtn = document.querySelector("#controls > a:nth-child(1)");
+const downBtn = document.querySelector("#controls > a:nth-child(2)");
+const main = document.querySelectorAll("#content > div");
+const start = document.querySelector("#introduction");
+const end = document.querySelector("footer");
+const chapter = [start, ...main, end]
+let index = 0
+
+const goToChapter = (direction) => {
+  const way = direction === "up" ? 1 : -1;
+  const newIndex = Math.max(0, Math.min(chapter.length - 1, index + way));
+
+  if (newIndex !== index) {
+    index = newIndex;
+    chapter[index].scrollIntoView({ behavior: 'smooth', block: 'start'});
+  }}
+
+upBtn.addEventListener("click", () => goToChapter("up"));
+downBtn.addEventListener("click", () => goToChapter("down"));
+downloadBtn.addEventListener("click", () => {
+  chapter[3].scrollIntoView({ behavior: 'smooth', block: 'start'});
+  index = 3;
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowUp") {
+    event.preventDefault();
+    goToChapter("down");
+  } else if (event.key === "ArrowDown") {
+    event.preventDefault();
+    goToChapter("up");
+  };
+});
+
+questions.forEach((question) => {
+    question.addEventListener("click", () => {
+        question.querySelector(".arrow").classList.toggle("active");
+        question.querySelector(".answer").classList.toggle("visible");
+    });
+});
