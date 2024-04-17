17297c0e52f52aebdddbe861f3e16f4e4d10de4d
Ethan <QustoTemmo@protonmail.com>
2024-04-17 01:56:58 +0100

    Some Adjustment

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const questions = document.querySelectorAll(".question");
-const downloadBtn = document.querySelector(".download")
+const downloadBtn = document.querySelector(".download");
 const upBtn = document.querySelector("#controls > a:nth-child(1)");
 const downBtn = document.querySelector("#controls > a:nth-child(2)");
 const main = document.querySelectorAll("#content > div");
 const start = document.querySelector("#introduction");
 const end = document.querySelector("footer");
-const chapter = [start, ...main, end]
-let index = 0
+const chapters = [start, ...main, end];
+let index = 0;
 
 const goToChapter = (direction) => {
   const way = direction === "up" ? 1 : -1;
-  const newIndex = Math.max(0, Math.min(chapter.length - 1, index + way));
+  const newIndex = Math.max(0, Math.min(chapters.length - 1, index + way));
 
   if (newIndex !== index) {
     index = newIndex;
-    chapter[index].scrollIntoView({ behavior: 'smooth', block: 'start'});
-  }}
+    chapters[index].scrollIntoView({ behavior: 'smooth', block: 'start'});
+  };
+};
 
 upBtn.addEventListener("click", () => goToChapter("up"));
-downBtn.addEventListener("click", () => goToChapter("down"));
+downBtn.addEventListener("click", () => goToChapter());
 downloadBtn.addEventListener("click", () => {
-  chapter[3].scrollIntoView({ behavior: 'smooth', block: 'start'});
   index = 3;
+  chapters[3].scrollIntoView({ behavior: 'smooth', block: 'start'});
 });
 
 document.addEventListener("keydown", (event) => {
