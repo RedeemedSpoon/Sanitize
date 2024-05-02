00d8083e140339f6bbcd76f29eec0f35ff3d86b4
Ethan <QustoTemmo@protonmail.com>
2024-05-02 19:29:11 +0100

    Load/Store Settings

--- src/popup.js
+++ src/popup.js
@@ -1,27 +1,61 @@
+import {getSettings, setSettings, getUrl, initOptSettings, toggleOptSettings, resetSettings} from './utils.js';
+
 const allSettings = document.querySelectorAll('input');
-const exportBtn = document.getElementById('export');
-const importBtn = document.getElementById('import');
+const optSettings = document.querySelectorAll('.optSetting');
+const resetBtn = document.getElementById('reset');
 const globalBtn = document.getElementById('global');
 const localBtn = document.getElementById('local');
-const audio = document.querySelector('audio');
-const logo = document.querySelector('img[alt="Zenify"]');
+const warning = document.getElementById('warning');
+const logo = document.getElementById('logo');
 
-logo.addEventListener('click', () => audio.play());
+// On Load
+document.addEventListener('DOMContentLoaded', () => reloadSettings());
+logo.addEventListener('click', () => new Audio('./yay.mp3').play());
 
+// Top Right Buttons
+resetBtn.addEventListener('click', async () => {
+  await resetSettings();
+  window.location.reload();
+});
+
+// Scope Buttons
 globalBtn.addEventListener('click', (e) => {
   e.preventDefault();
   globalBtn.classList.add('active');
   localBtn.classList.remove('active');
+  reloadSettings();
 });
 
 localBtn.addEventListener('click', (e) => {
   e.preventDefault();
   globalBtn.classList.remove('active');
   localBtn.classList.add('active');
+  reloadSettings();
 });
 
+// On Change / Click
 allSettings.forEach((setting) =>
-  setting.addEventListener('click', () => {
-    console.log({[setting.id]: setting.checked});
+  setting.addEventListener('click', async () => {
+    const url = globalBtn.classList.contains('active') ? 'global' : await getUrl();
+    await setSettings(url, setting.id, setting.checked);
+    warning.style.display = 'block';
+    console.log(await browser.storage.local.get());
   }),
 );
+
+optSettings.forEach((optSetting) => {
+  optSetting.addEventListener('click', () => toggleOptSettings(optSetting.id));
+});
+
+warning.addEventListener('click', () => {
+  warning.style.display = 'none';
+});
+
+// Functions
+const reloadSettings = async () => {
+  await initOptSettings([...optSettings]);
+  const url = globalBtn.classList.contains('active') ? 'global' : await getUrl();
+  allSettings.forEach(async (setting) => {
+    setting.checked = await getSettings(url, setting.id);
+  });
+};
---
+++ src/utils.js
@@ -0,0 +1,49 @@
+const getUrl = async () => {
+  const tabs = await browser.tabs.query({active: true, currentWindow: true});
+  return new URL(tabs[0].url).hostname;
+};
+
+const resetSettings = async () => {
+  await browser.storage.local.clear();
+};
+
+const initOptSettings = async (optSettings) => {
+  const settings = await browser.storage.local.get();
+
+  optSettings.forEach(async (optSetting) => {
+    if (!settings[optSetting.id]) {
+      settings[optSetting.id] = optSetting.id === 'activateExt' ? true : false;
+      await browser.storage.local.set(settings);
+    }
+  });
+};
+
+const toggleOptSettings = async (settingId) => {
+  const settings = await browser.storage.local.get();
+  settings[settingId] = !settings[settingId];
+  await browser.storage.local.set(settings);
+};
+
+const getSettings = async (scope, settingId) => {
+  const settings = await browser.storage.local.get();
+  return (settings[scope] && settings[scope].includes(settingId)) || false;
+};
+
+const setSettings = async (scope, settingId, newValue) => {
+  const settings = await browser.storage.local.get();
+  settings[scope] = settings[scope] || [];
+
+  if (newValue) {
+    settings[scope].push(settingId);
+  } else {
+    settings[scope] = settings[scope].filter((setting) => setting !== settingId);
+    if (!settings[scope].length) {
+      await browser.storage.local.remove(scope);
+      return;
+    }
+  }
+
+  await browser.storage.local.set(settings);
+};
+
+export {setSettings, getSettings, getUrl, initOptSettings, toggleOptSettings, resetSettings};
